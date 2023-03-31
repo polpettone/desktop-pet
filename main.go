2721660e38ee6c232eb050d3bed30411ef90d0f6
Stefan Heiden <stefanheiden@gmail.com>
2023-03-31 12:57:44 +0200

    initial

---
+++ main.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"log"
+	"os"
+	"strconv"
+
+	"github.com/hajimehoshi/ebiten"
+)
+
+const (
+	screenWidth  = 200
+	screenHeight = 200
+)
+
+func main() {
+
+	petImagesPath := "pet-2"
+	scale := 1.0
+	walkMode := Bottom
+	if len(os.Args) > 1 {
+		petImagesPath = os.Args[1]
+	}
+
+	if len(os.Args) > 2 {
+		scaleFromArg, err := strconv.ParseFloat(os.Args[2], 64)
+		if err != nil {
+			log.Fatal(err)
+		}
+		scale = scaleFromArg
+	}
+
+	if len(os.Args) > 3 {
+		if os.Args[3] == "RANDOM" {
+			walkMode = Random
+		}
+	}
+
+	pet := NewPet(50, 50, scale, petImagesPath)
+
+	ebiten.SetWindowSize(screenWidth, screenHeight)
+	ebiten.SetWindowTitle("Desktop Pet")
+	ebiten.SetRunnableOnUnfocused(true)
+	ebiten.SetWindowPosition(1000, 1000)
+	ebiten.SetWindowDecorated(false)
+	ebiten.SetWindowFloating(true)
+
+	game := &Game{
+		Pet:      pet,
+		X:        500,
+		Y:        500,
+		Clock:    0,
+		WalkMode: walkMode,
+	}
+	ebiten.SetScreenTransparent(true)
+	if err := ebiten.RunGame(game); err != nil {
+		log.Fatal(err)
+	}
+}
---
+++ pet.go
@@ -0,0 +1,111 @@
+package main
+
+import (
+	"image"
+	"log"
+	"os"
+
+	"github.com/hajimehoshi/ebiten"
+	"github.com/hajimehoshi/ebiten/ebitenutil"
+	_ "github.com/polpettone/desktop-pet/statik"
+	"github.com/rakyll/statik/fs"
+)
+
+type Pet struct {
+	X                int
+	Y                int
+	Images           []*ebiten.Image
+	CurrentImage     *ebiten.Image
+	AnimationCounter int
+	Scale            float64
+}
+
+func NewPet(x, y int, scale float64, path string) *Pet {
+
+	images, err := loadImagesFromStatik()
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return &Pet{
+		X:                x,
+		Y:                y,
+		Images:           images,
+		AnimationCounter: 0,
+		CurrentImage:     images[0],
+		Scale:            scale,
+	}
+}
+
+func (p *Pet) Draw(screen *ebiten.Image, gameClock int) {
+
+	op := &ebiten.DrawImageOptions{}
+
+	op.GeoM.Scale(p.Scale, p.Scale)
+	op.GeoM.Translate(float64(p.X), float64(p.Y))
+
+	screen.DrawImage(p.CurrentImage, op)
+
+	if gameClock%40 == 0 {
+		p.CurrentImage = p.Images[p.AnimationCounter]
+		p.AnimationCounter = p.AnimationCounter + 1
+		if p.AnimationCounter >= len(p.Images) {
+			p.AnimationCounter = 0
+		}
+	}
+
+}
+
+func loadImagesFromStatik() ([]*ebiten.Image, error) {
+	images := []*ebiten.Image{}
+
+	files := []string{"hamster-1.png", "hamster-2.png", "hamster-2.png", "hamster-4.png"}
+
+	statikFS, err := fs.New()
+	if err != nil {
+		return nil, err
+	}
+
+	path := "pet-2"
+
+	for _, file := range files {
+
+		filePath := "/" + path + "/" + file
+		log.Printf("open %s", filePath)
+		r, err := statikFS.Open(filePath)
+		if err != nil {
+			return nil, err
+		}
+		defer r.Close()
+
+		img, _, err := image.Decode(r)
+		img2, err := ebiten.NewImageFromImage(img, ebiten.FilterDefault)
+
+		if err != nil {
+			return nil, err
+		}
+		images = append(images, img2)
+	}
+
+	return images, nil
+}
+
+func loadImages(path string) ([]*ebiten.Image, error) {
+	images := []*ebiten.Image{}
+
+	files, err := os.ReadDir(path)
+	if err != nil {
+		return nil, err
+	}
+
+	for _, file := range files {
+		img, _, err := ebitenutil.NewImageFromFile(path+"/"+file.Name(), ebiten.FilterDefault)
+		if err != nil {
+			return nil, err
+		}
+		images = append(images, img)
+	}
+
+	return images, nil
+}
